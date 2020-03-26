422fa3d3a8486da6f0353b70b154ba64ea5e9525
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-03-26 10:34:04 -0300

    Add Logon Page

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logon from '../../pages/Logon';
+
+// import { Container } from './styles';
+
+export default function Header({title, children}) {
+  return (
+    <>
+      <Logon />
+    </>
+  );
+}
